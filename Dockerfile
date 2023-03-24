98704de995b37d721f930fcd9406617c2ce7c8c2
문상헌 <shmoon@toss.im>
2023-03-24 20:24:35 +0900

    feat: add django app

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,9 @@ FROM python:3.11
 WORKDIR /code
 
 COPY ./requirements.txt /code/requirements.txt
-
-RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+RUN pip install poetry
+RUN poetry install
 
 COPY ./app /code/app
-
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
 
--- tenseconds/django_project/settings.py
+++ tenseconds/django_project/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "tenseconds.urls"
+ROOT_URLCONF = "urls"
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "tenseconds.wsgi.application"
+WSGI_APPLICATION = "django_project.wsgi.application"
 
 
 # Database
